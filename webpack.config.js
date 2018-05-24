0e5932cab51a6db4bd0745be8d12b9f6fd31f0c0
Cindy <c.puchex@gmail.com>
2018-05-23 23:12:20 -0400

    drawing branch properly

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: "./lib/tree.js",
+  entry: "./lib/entry.js",
   output: {
       path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
       filename: "bundle.js"
